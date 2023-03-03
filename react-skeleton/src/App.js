ba1ba3717f02f3067486f0a261265a798dad3220
dsr <d.singhranger@yahoo.co.uk>
2023-03-03 22:28:27 +0000

    basic skeleton of Homepage

---
+++ react-skeleton/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Bootstrap from "./components/Bootstrap"
+
+function App() {    
+    return (
+    <Bootstrap />
+    )
+}
+export default App
---
+++ react-skeleton/src/components/Bootstrap.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+import Image from "./Images/Image"
+import Card from "./Cards/Card"
+
+function Bootstrap() {
+    return ( <>
+        <Navbar />
+        <Image />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </>
+        )
+}
+export default Bootstrap
