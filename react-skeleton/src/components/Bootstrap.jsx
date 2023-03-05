7dad9ceef7360a1a8fca6adc1b0a879b7b58a422
dsr <d.singhranger@yahoo.co.uk>
2023-03-05 00:22:23 +0000

    images added using cardimages array in json file(created)

--- react-skeleton/src/components/Bootstrap.jsx
+++ react-skeleton/src/components/Bootstrap.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Navbar from './Navbar/Navbar';
-import Image from "./Images/Image"
-import Card from "./Cards/Card"
+import Navbar from './Navbar/index';
+import Image from "./Images/index"
+import Card from "./Cards/index"
+import cardimages from "../cardimages.json"
 
 function Bootstrap() {
     return ( <>
         <Navbar />
         <Image />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card image={cardimages[0].image} name={cardimages[0].name}/>   
+        <Card image={cardimages[1].image} name={cardimages[1].name}/>
+        <Card image={cardimages[2].image} name={cardimages[2].name}/>
+        <Card image={cardimages[3].image} name={cardimages[3].name}/>
+        <Card image={cardimages[4].image} name={cardimages[4].name}/>
         </>
         )
 }
--- react-skeleton/src/components/Cards/index.jsx
+++ react-skeleton/src/components/Cards/index.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 
-function Card () {
+function Card (props) {
     return(
         <>
         <div className="card" >
-  <img src="..." className="card-img-top" alt="..."/>
+  <img src= {props.image} className="card-img-top" alt={props.name}/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
